512c6b05bf186afe9e7c225fb47a70524ece3780
Kushagra Anand <kushrokz95@gmail.com>
2020-06-03 20:11:22 +0530

    1st attempt

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(function (req, res, next) {
 app.use(transaction);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use("/", express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
